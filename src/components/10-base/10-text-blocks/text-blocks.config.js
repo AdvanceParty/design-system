df12146465f2d7c01da8cd846e8b2e65e633aefc
Gil Fewster <hello@gilfewster.com>
2019-05-27 14:36:09 +1000

    improved system for vertical rhythm

--- src/components/10-base/10-text-blocks/text-blocks.config.js
+++ src/components/10-base/10-text-blocks/text-blocks.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   context: {
     content:
-      'LOCAL Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet tempus quam. Ut est nisi, facilisis nec iaculis sed, tempor vel diam.Pellentesque gravida mollis lacus suscipit pretium. Fusce sed lectus ut tortor finibus tincidunt sit amet at tellus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet tempus quam. Ut est nisi, facilisis nec iaculis sed, tempor vel diam.Pellentesque gravida mollis lacus suscipit pretium. Fusce sed lectus ut tortor finibus tincidunt sit amet at tellus.',
   },
 };
